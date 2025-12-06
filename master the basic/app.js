49835585e382dba655b32b7e1d8bb01c92105e24
shreyash Jadhav <connectshreyashj96@gmail.com>
2025-12-06 11:57:11 +0530

    first proble

---
+++ master the basic/app.js
@@ -0,0 +1,9 @@
+// Ṛelation between string and number
+let a = 10
+let b = '20' // number + string its reture a string 
+
+console.log(typeof(a+b)) // this is not addation | this is concatinations 
+
+// sum and message
+console.log('sum of 10 and 20 is ' + a + b)
+

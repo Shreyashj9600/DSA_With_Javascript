239eab6862899c901f62f70f57aeb470e2663ca6
shreyash Jadhav <connectshreyashj96@gmail.com>
2026-03-10 11:30:48 +0530

    match fun

--- 01master the basic/02index.js
+++ 01master the basic/02index.js
@@ -1,3 +1,18 @@
 let a = 4567
 
 console.log(a%10);
+
+
+let i = 10
+
+let j = i++
+
+console.log(i);
+console.log(j)
+
+
+// math fun
+console.log(Math.round(10.3)); // 
+console.log(Math.ceil(10.3)); 
+console.log(Math.floor(10.3)); 
+console.log(Math.trunc(10.3)); 

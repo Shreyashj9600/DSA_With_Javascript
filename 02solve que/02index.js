9be18b922065fa75dafc1ce340a00382464d3283
shreyash Jadhav <connectshreyashj96@gmail.com>
2026-03-25 10:44:06 +0530

    pass condiction in swithc case

--- 02solve que/02index.js
+++ 02solve que/02index.js
@@ -8,6 +8,18 @@ switch (day) {
         break
 
     default: console.log('invalid');
+}
 
+switch (true) {
+    case 11 < 6:
+        console.log('hello');
+        break
+
+    case 10 > 9:
+        console.log('hey');
+        break
+        
+    default:
+        console.log('invalid');
 
 }
